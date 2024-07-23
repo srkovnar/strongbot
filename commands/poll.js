b40849b49794acbece837c1ad8db088acce962a7
Sam <srkovnar@gmail.com>
2024-07-22 21:55:04 -0500

    Fix description to be on one line

--- commands/poll.js
+++ commands/poll.js
@@ -45,7 +45,7 @@ module.exports = {
         .addStringOption(option =>
             option
             .setName("description")
-            .setDescription("A description for your poll that will appear directly beneath the title.")
+            .setDescription("A description for your poll that will appear directly under the title.")
             .setMaxLength(50)
         )
     ,
@@ -56,20 +56,6 @@ module.exports = {
         const { user, guild, channel } = await interaction;
         const options = await interaction.options.data;
 
-        const emojis = [
-            ":one:",
-            ":two:",
-            ":three:",
-            ":four:",
-            ":five:"
-        ];
-        const emoji_names = [
-            "one",
-            "two",
-            "three",
-            "four",
-            "five"
-        ]
         const emoji_stupid_ugly_array = [
             "1️⃣",
             "2️⃣",
@@ -78,13 +64,29 @@ module.exports = {
             "5️⃣"
         ]//WHY IS THIS THE ONLY WAY THAT WORKS. THAT'S SO STUPID.
 
+        let emoji_structure = [
+            {name: "one",   index: 0,   emoji: "1️⃣", argname: "option1" },
+            {name: "two",   index: 1,   emoji: "2️⃣", argname: "option2" },
+            {name: "three", index: 2,   emoji: "3️⃣", argname: "option3" },
+            {name: "four",  index: 3,   emoji: "4️⃣", argname: "option4" },
+            {name: "five",  index: 4,   emoji: "5️⃣", argname: "option5" }
+        ]
+        /*  This is set to non-constant because at some point I would like to introduce
+        *   the ability to set custom poll reactions as command arguments.
+        *
+        *   I would also like to pair these emojis directly with the argument name
+        *   (option1, option2, etc.) instead of the index in the array.
+        *   That's why the "argname" field exists, despite the fact that it is unused.
+        */
+
         let embed = new EmbedBuilder() // Still don't understand const vs let
             .setTitle("Poll")
             .setColor("Red")
         ;
 
         for (let i = 0; i < (options.length); i++) {
-            let emoji = emojis[i];
+            //let emoji = emojis[i];
+            let emoji_entry = emoji_structure.find(target => target.index === i);
             let option = options[i];
 
             if (option.name == "title") {
@@ -96,7 +98,8 @@ module.exports = {
             else {
                 embed.addFields(
                     {
-                        name: `${emoji} - ${option.value}`,
+                        //name: `${emoji} - ${option.value}`,
+                        name: `${emoji_entry.emoji} - ${option.value}`,
                         value: " "
                     }
                 );
@@ -105,32 +108,26 @@ module.exports = {
 
         const message = await channel.send({embeds: [embed]});
 
-        console.log(`HERE IS THE EMOJI STRUCTURE: ${message.guild.emojis.cache}`);
+        //console.log(`HERE IS THE EMOJI STRUCTURE: ${message.guild.emojis.cache}`);
 
         for (let k = 0; k < (options.length); k++) {
             let option = options[k];
             //let class_emoji = new Emoji(client, "one");
             if ((option.name != "title") && (option.name != "description"))
             {
-                let emoji = message.guild.emojis.cache.find(emoji => emoji.name === emoji_names[k]);
-                //let emoji = guild.emojis.cache.find(emoji => emoji.name === "one");
-                
-                //console.log(emoji);
+                /* This was my original implementation of creating the reaction emojis. */
+                //message.react(emoji_stupid_ugly_array[k]);
 
-                //let poo_emoji = new Emoji(interaction.client, emoji);
-                // I thought maybe you're supposed to use the Emoji class. That doesn't seem to be the case.
-
-                //message.react("one");
-                //message.react(emoji);
-                
-                //console.log(emoji_stupid_ugly_array[k].name);
-
-                message.react(emoji_stupid_ugly_array[k]);
-                //message.react(emoji);
-                //message.react(emoji.id); // No, didn't work.
+                /* I tried a lot of things to make this cleaner,
+                *  but I had difficulty finding any way to target an emoji by its name.
+                *  I eventually settled on creating a structure to hold the reaciton emojis.
+                *  That implementation is shown below.
+                */
+                let emoji_entry = emoji_structure.find(target => target.index === k);
+                message.react(emoji_entry.emoji);
             }
         }
 
-        await interaction.editReply({content: 'sent poll successfully'});
+        await interaction.editReply({content: 'sent poll successfully'}); // Done deferring
     }
 }
